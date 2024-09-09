3d4becab918fa3b1ad741867322c604a791767db
Ndekocode <arickbulakali@gmail.com>
2024-09-09 12:41:36 +0200

    feat: button motion

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -1,15 +1,24 @@
 import {FC, PropsWithChildren} from "react";
+import {motion} from 'framer-motion'
 
 export const Btn: FC<PropsWithChildren<{ onClick: () => void }>> = ({
                                                                         children,
                                                                         ...props
                                                                     }) => {
     return (
-        <button
+        <motion.button
             className="px-4 py-2 text-sm text-white bg-blue-500 rounded-md"
+            animate={{
+                scale: 1.5,
+            }}
+            transition={{
+                repeat: 2,
+                ease: "easeInOut",
+                repeatType: 'reverse'
+            }}
             {...props}
         >
             {children}
-        </button>
+        </motion.button>
     );
 };
