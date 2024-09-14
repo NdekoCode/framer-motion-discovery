a435c70663e88a6829388b2f78806398dc87831e
Ndekocode <arickbulakali@gmail.com>
2024-09-14 11:08:08 +0200

    refactor: add use spring with framer-motion

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,15 @@
 'use client';
 
-import {motion} from "framer-motion";
+import {motion, useMotionValue, useSpring} from "framer-motion";
+import React from "react";
 
 const About = () => {
+    const scale = useMotionValue(1);
+    const scaleSpring = useSpring(scale);
+
+    const onRangeChange = (e: ChangeEvent<HTMLInputElement>) => {
+        scaleSpring.set(parseFloat(e.target.value));
+    }
     return (
         <div className="flex flex-col gap-y-10">
             <h1 className="mb-3 text-3xl text-gray-900 font-bold">About us</h1>
@@ -19,6 +26,22 @@ const About = () => {
                 ease: "easeInOut"
             }} className="size-40 bg-gray-700 flex items-center justify-center rounded"><span
                 className="text-xl font-medium text-gray-100">Hello</span></motion.div>
+            <div className="flex flex-col gap-y-10 h-full mt-auto">
+
+
+                <motion.button
+                    className="bg-black text-white w-max px-3 py-2 rounded-md"
+                    style={{scale: scaleSpring}}
+                    transition={{type: 'spring', damping: 300}}
+
+                >
+                    Scale ⚖️
+                </motion.button>
+                <div className="w-max mt-auto">
+                    <input type="range" min={0.5} max={10} defaultValue={1}
+                           onChange={onRangeChange}/>
+                </div>
+            </div>
         </div>
     );
 };
