be4a5bb0ca9a26b573f4c46132855064f5c76bd9
Ndekocode <arickbulakali@gmail.com>
2024-09-15 17:18:30 +0200

    docs: add while-in-view and useInView hook

--- app/basic/page.tsx
+++ app/basic/page.tsx
@@ -1,14 +1,15 @@
-// import BasicsOfMotion from '@/components/tom-is-loading/BasicsOfMotion';
-// import Gestures from '@/components/tom-is-loading/Gestures';
-
 import AnimationControls from '@/components/tom-is-loading/AnimationControls';
+import BasicsOfMotion from '@/components/tom-is-loading/BasicsOfMotion';
+import Gestures from '@/components/tom-is-loading/Gestures';
+import ViewBasedAnimations from '@/components/tom-is-loading/ViewBasedAnimations';
 
 const page = () => {
   return (
-    <div className="h-screen grid content-center gap-5">
-      {/* <BasicsOfMotion /> */}
-      {/* <Gestures/> */}
+    <div className="h-[200vh] min-h-screen grid content-center gap-5">
+      <BasicsOfMotion />
+      <Gestures/>
       <AnimationControls />
+      <ViewBasedAnimations />
     </div>
   );
 };
