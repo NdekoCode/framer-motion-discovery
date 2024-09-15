86ce9a865c7ceafa3a20f73fd7452117ac88c2e6
Ndekocode <arickbulakali@gmail.com>
2024-09-15 11:42:28 +0200

    refactor: add basics of motion

---
+++ app/basic/page.tsx
@@ -0,0 +1,11 @@
+import BasicsOfMotion from '@/components/tom-is-loading/BasicsOfMotion';
+
+const page = () => {
+  return (
+    <div className="h-screen grid content-center gap-5">
+      <BasicsOfMotion />
+    </div>
+  );
+};
+
+export default page;
