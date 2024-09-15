545b9c2c91aa94f5304fa19ff8d5ea9f8c016b5b
Ndekocode <arickbulakali@gmail.com>
2024-09-15 12:50:18 +0200

    feat: add gesture

--- app/basic/page.tsx
+++ app/basic/page.tsx
@@ -1,9 +1,11 @@
 import BasicsOfMotion from '@/components/tom-is-loading/BasicsOfMotion';
+import Gestures from '@/components/tom-is-loading/Gestures';
 
 const page = () => {
   return (
     <div className="h-screen grid content-center gap-5">
       <BasicsOfMotion />
+      <Gestures/>
     </div>
   );
 };
