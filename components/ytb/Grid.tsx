a239059ce247cf8a49af4dc1fc830fa179eefa13
Ndekocode <arickbulakali@gmail.com>
2024-09-16 09:53:53 +0200

    feat: add fadeUp and fadeDown animation

--- components/ytb/Grid.tsx
+++ components/ytb/Grid.tsx
@@ -16,6 +16,37 @@ const gridItemVariant = {
     opacity: 1,
   },
 };
+const fadeUp = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  fade: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      ease:"easeOut",
+      delay:0.25
+    },
+  },
+};
+
+const fadeDown = {
+  initial: {
+    opacity: 0,
+    y: -100,
+  },
+  fade: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      ease:"easeOut",
+      delay:0.4
+    },
+  },
+};
 const Grid = () => {
   return (
     <motion.div
@@ -24,10 +55,23 @@ const Grid = () => {
       animate="visible"
       className="grid grid-cols-3 p-10 gap-10"
     >
+      {/* Fade Up/Fade Down */}
       <motion.div
         variants={gridItemVariant}
         className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
-      ></motion.div>
+      >
+        {/* Fade Up */}
+        <motion.div
+          variants={fadeUp}
+          className="size-20 aspect-square bg-stone-100 rounded-lg"
+          initial="initial"
+          animate="fade"
+        />
+        {/* Fade Down */}
+        <motion.div variants={fadeDown}
+          initial="initial"
+          animate="fade" className="size-20 aspect-square bg-stone-100 rounded-full" />
+      </motion.div>
       <motion.div
         variants={gridItemVariant}
         className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
