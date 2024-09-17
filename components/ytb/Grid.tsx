035e7907d15c9581aa76213843668476ffdcc169
Ndekocode <arickbulakali@gmail.com>
2024-09-17 07:46:35 +0200

    feat: add svg animation

--- components/ytb/Grid.tsx
+++ components/ytb/Grid.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, Variants } from 'framer-motion';
+import { motion, useScroll, Variants } from 'framer-motion';
 
 const gridVariants: Variants = {
   hidden: { opacity: 0 },
@@ -57,7 +57,39 @@ const tapAndHover = {
     scale: 0.95,
   },
 };
+
+const svgIconVariant = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(252,211,77,0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(252,211,77,1)",
+    transition: {
+      default: {
+        duration: 1,
+        ease: "easeInOut",
+        delay: 0.5,
+        repeat: Infinity,
+        repeatType: "reverse",
+        repeatDelay: 0.5,
+      },
+      fill: {
+        duration: 1,
+        ease: "easeIn",
+        delay: 0.5,
+        repeat: Infinity,
+        repeatType: "reverse",
+        repeatDelay: 0.5,
+      },
+    },
+  },
+};
 const Grid = () => {
+  const { scrollYProgress: completionProgress } = useScroll();
   return (
     <motion.div
       variants={gridVariants}
@@ -144,14 +176,36 @@ const Grid = () => {
           className="size-1/3 bg-orange-500 rounded-3xl cursor-grab"
         />
       </motion.div>
+      {/* Scroll animation */}
       <motion.div
         variants={gridItemVariant}
         className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
-      ></motion.div>
+      >
+        <motion.div className="size-40 aspect-square bg-gray-50/20 rounded-xl">
+          <motion.div
+            className="size-full bg-gray-400 rounded-xl origin-bottom"
+            style={{ scaleY: completionProgress }}
+          />
+        </motion.div>
+      </motion.div>
+      {/* SVG Animation */}
       <motion.div
         variants={gridItemVariant}
         className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
-      ></motion.div>
+      >
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="w-1/2 stroke-amber-500 stroke-[0.5]"
+        >
+          <motion.path
+            variants={svgIconVariant}
+            initial="hidden"
+            animate="visible"
+            d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+          />
+        </motion.svg>
+      </motion.div>
     </motion.div>
   );
 };
