0c7504768741f9962dd18b29188ff9b2eca8ac3f
Arick Bulakali <arickbulakali@gmail.com>
2024-11-29 06:39:30 +0200

    feat : add scroll based on parent component

--- app/rithmic/page.tsx
+++ app/rithmic/page.tsx
@@ -1,5 +1,8 @@
 "use client";
-import { motion, useScroll, Variants } from 'motion/react';
+import { motion, useAnimationControls, useInView, useScroll, Variants } from 'motion/react';
+import { useEffect, useRef } from 'react';
+
+import ScrollBlock from '@/components/ScrollBlock';
 
 const showHiddenVariant: Variants = {
   hidden: {
@@ -14,6 +17,16 @@ const showHiddenVariant: Variants = {
       // Ici je dis quand l'element va etre visible je veux ajouter un retard d'animation de 0.25s sur chaque element enfant de l'element parent
       staggerChildren: 0.25,
     },
+  },
+};
+const containerGradient: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 75,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
   },
 };
 const svgVariant = {
@@ -40,13 +53,24 @@ const gridItemVariant = {
 };
 const page = () => {
   const { scrollYProgress } = useScroll();
+  // On creer une reference que l'on va ecouter quand il sera present dans la vue (Quand il sera visible dans la vue(ecran) de l'utilisateur)
+  const containerRef = useRef<HTMLDivElement>(null);
+  // On creer une valeurs qui changera quand l'element HTML referencer par `containerRef` sera visible dans la vue de l'utilisateur, on lui passe l'element a observer mais on dit que on veut que cela soit detecter une seule fois
+  const isInView = useInView(containerRef, { once: true });
+  // On definit un controlleur manuelle des animations framer-motion
+  const mainControl = useAnimationControls();
+  useEffect(() => {
+    if (isInView) {
+      mainControl.start("visible");
+    }
+  }, [isInView]);
   return (
     <div className="dark">
-      <div className="container">
+      <div className="container mt-10">
+        <div className="flex flex-col overflow-x-hidden gap-10">
         <h1 className="text-3xl text-gray-700 dark:text-gray-100">
           Learn Framer Motion with Rithmic
         </h1>
-        <div className="flex flex-col overflow-x-hidden gap-10">
           <motion.div
             variants={showHiddenVariant}
             initial="hidden"
@@ -202,166 +226,18 @@ const page = () => {
               </motion.svg>
             </motion.div>
           </motion.div>
-          <p>
-            <span>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
-              veniam odio atque hic, labore quos consequatur architecto
-              suscipit, sint corporis alias est? Nisi, totam dolorum. Nostrum
-              qui adipisci praesentium deserunt!
-            </span>
-            <span>
-              Pariatur sint adipisci architecto odio possimus voluptatibus,
-              quasi quibusdam laborum sapiente ad aut maxime. Accusamus sequi
-              ullam id harum architecto dicta laudantium ipsum vitae delectus
-              aliquid similique, dolores rerum magnam?
-            </span>
-            <span>
-              Excepturi mollitia voluptas, cumque, quis odit facilis asperiores
-              quos repudiandae placeat praesentium, sit deleniti sunt? Ex
-              maiores molestiae saepe modi architecto cumque? Illum numquam
-              aliquam expedita neque, qui amet pariatur.
-            </span>
-            <span>
-              Temporibus expedita autem quia exercitationem delectus iusto eos
-              ex accusantium dolor. Incidunt et earum pariatur dolorum delectus
-              nobis amet, ipsa reprehenderit error debitis in suscipit possimus,
-              numquam asperiores quam quisquam.
-            </span>
-            <span>
-              Suscipit neque aliquam error distinctio ad minus deleniti quas
-              doloribus architecto optio? Quidem, sed praesentium alias omnis id
-              nisi, vitae molestiae ipsa natus repellat minus dolore corrupti
-              nemo, aliquam cupiditate?
-            </span>
-          </p>
-          <p>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-              harum nemo eveniet. Id consequuntur ea itaque rerum quaerat.
-              Laudantium provident repellendus hic accusantium qui labore facere
-              corrupti tenetur aperiam nulla.
-            </span>
-            <span>
-              A ipsa illo, ad voluptate deserunt facere ratione dicta iste
-              similique voluptatibus quisquam quae itaque voluptatum sunt. Modi
-              quas saepe, sit impedit cum fugiat, iure non laudantium quibusdam
-              quia eaque?
-            </span>
-            <span>
-              Quisquam deleniti error sequi quae labore, rem possimus?
-              Reiciendis cum commodi, fugit cupiditate explicabo deserunt
-              exercitationem magnam rerum, sapiente doloremque ducimus sit alias
-              illum? Vero at inventore explicabo ea sunt!
-            </span>
-            <span>
-              Facere voluptate a obcaecati ut vero qui fugit officiis quaerat
-              quasi, aperiam rem quae dolorem nam dignissimos? Beatae ad libero
-              unde. Eligendi nisi a, sit ipsam facere sunt magni sint.
-            </span>
-            <span>
-              Nostrum, obcaecati fugit maiores corrupti molestiae quam,
-              exercitationem provident, ipsum eos iure possimus neque deleniti
-              reprehenderit nulla id adipisci illo molestias totam aspernatur
-              alias voluptatibus itaque perferendis repudiandae unde.
-              Voluptates.
-            </span>
-          </p>
-          <p>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-              dolores quis repellendus nisi delectus blanditiis, esse tempora
-              doloremque itaque, enim corporis similique fugiat cupiditate hic
-              iure laborum in temporibus optio?
-            </span>
-            <span>
-              Accusamus, magni nostrum, neque quo dignissimos consectetur
-              commodi laudantium minus, reprehenderit iure maiores pariatur?
-              Praesentium deserunt quam illo, hic, quia non animi odio optio,
-              autem enim consequuntur quasi molestiae consectetur.
-            </span>
-            <span>
-              Eum earum labore, soluta iste numquam, optio corrupti deleniti
-              accusamus possimus quaerat minus delectus a, dolorum voluptatibus
-              dolor suscipit itaque eos. Optio aperiam ab ipsam laudantium rerum
-              accusantium cupiditate exercitationem.
-            </span>
-            <span>
-              Non veritatis ipsa cupiditate, odit aspernatur vero possimus
-              cumque repellat maiores laboriosam doloribus a ea, cum eaque
-              deleniti, consequuntur culpa odio quos. Iure dolor nihil atque
-              veniam esse ducimus ex.
-            </span>
-            <span>
-              Alias id culpa officia facilis? Quo, obcaecati ut, ratione sit
-              aliquam deserunt eius architecto atque quaerat porro fugiat
-              perspiciatis sed maiores vel sint vitae eum, repellat odio autem
-              nam eos?
-            </span>
-          </p>
-          <p>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Repellendus, eum dignissimos. Assumenda mollitia tenetur adipisci
-              id expedita culpa repellendus? Maiores animi corporis natus
-              voluptatum pariatur atque explicabo at praesentium totam.
-            </span>
-            <span>
-              Delectus architecto fugit quaerat repellendus eaque provident non
-              culpa id ducimus sapiente obcaecati at natus, voluptatum quod
-              eligendi unde voluptate in ipsam. Iste, dolorum. Ipsam,
-              perferendis. Soluta eum quia eligendi.
-            </span>
-            <span>
-              Nulla culpa veritatis corporis, perferendis assumenda excepturi ab
-              id quaerat facilis error impedit nisi numquam blanditiis dolor
-              ipsa voluptates sequi ullam aliquid temporibus dolore. Animi illo
-              est nostrum quos quasi.
-            </span>
-            <span>
-              Voluptas, commodi! Officiis sapiente odit eaque eveniet, cum alias
-              officia fuga. Voluptate, necessitatibus neque praesentium beatae
-              et molestias, eum consequatur minima doloremque aperiam ullam quis
-              tempore? Officia minima vero fugit.
-            </span>
-            <span>
-              Itaque assumenda quibusdam ipsum nihil, placeat laudantium earum
-              iste? Ratione, soluta ipsam quibusdam eius sunt natus doloribus
-              beatae, veritatis ea nobis ipsa deserunt adipisci cupiditate
-              possimus voluptatibus nulla et expedita.
-            </span>
-          </p>
-          <p>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-              alias qui quis sapiente sed eos, est corrupti voluptatem sunt
-              minus dignissimos, a quisquam odit rem provident doloribus ipsa
-              suscipit temporibus.
-            </span>
-            <span>
-              Sapiente, ad esse pariatur harum ab maxime non deleniti, quos
-              natus aspernatur nesciunt eaque consectetur mollitia in qui omnis
-              iste cum molestias aliquam! Est quibusdam esse facilis nemo
-              inventore sapiente!
-            </span>
-            <span>
-              Eum aliquid ducimus, dolores maiores aperiam ab earum et eaque.
-              Perspiciatis sunt temporibus quia sequi ipsam fuga sed recusandae
-              deleniti repellat eveniet, iusto deserunt rerum vero reiciendis
-              magni. Error, assumenda.
-            </span>
-            <span>
-              Quae soluta atque sequi nihil maxime, ducimus quos sint quia
-              facere, vitae inventore illo vero alias facilis modi debitis
-              ratione laudantium saepe enim deserunt, mollitia quo nisi beatae
-              veniam. Blanditiis.
-            </span>
-            <span>
-              Eveniet vitae repudiandae consectetur itaque distinctio
-              perferendis explicabo dolore deleniti tempora doloribus at modi
-              nemo provident fuga aliquam ducimus sequi illo qui, enim ipsam
-              deserunt aperiam quia. Eos, quas impedit.
-            </span>
-          </p>
+          <section className="flex flex-col gap-10" ref={containerRef}>
+            <motion.h1
+              variants={containerGradient}
+              initial="hidden"
+              transition={{ delay: 0.3 }}
+              className="text-4xl text-gray-100 font-semibold"
+              animate={mainControl}
+            >
+              Just keep scrolling
+            </motion.h1>
+            <ScrollBlock containerRef={containerRef}/>
+          </section>
         </div>
       </div>
     </div>
